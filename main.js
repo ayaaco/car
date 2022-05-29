abdf820af8d45cfda0c892a62209e0b4248d1c58
ayaaco <106485710+ayaaco@users.noreply.github.com>
2022-05-29 12:57:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function change(){
+    document.getElementById("img1").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlb4wl1zuw3xnwIWFOMYhOUiLMzgfWatsLyvLDCNsJyRiiEjzy4fsH7mQTgyqA5_d6NyU&usqp=CAU";
+}
